969e267ca8409162196f28b8c852a60be9e5c46e
Gabriel Assana <gabrielhhz2@gmail.com>
2022-09-11 23:32:33 -0300

    Crianção e estilização do Formulario de SignIn

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 
-import { SignIn } from './pages/Home';
+import { SignIn } from './pages/SignIn';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
 
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,37 @@
-import { Container } from './styles';
+import { FiLock, FiMail } from 'react-icons/fi';
+
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
+import { Container, Form } from './styles';
 
 export function SignIn() {
     return(
         <Container>
-            
+            <Form>
+                <h1> Rocket Notes</h1>
+                <p>Alplicação para slavar e gerenciar seus links úteis.</p>
+
+                <h2>Faça seu login</h2>
+
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+
+                <Input
+                    placeholder="E-mail"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button title="Entrar"/>
+
+                <a href="#">
+                    Criar Conta
+                </a>
+
+            </Form>
         </Container>
     )
 }
