77da98a88ba7259396e070b7bd17e509bbcb2da5
Gabriel Assana <gabrielhhz2@gmail.com>
2022-09-15 23:54:53 -0300

    Criando componente Textarea

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
 import { Container, Form } from './styles';
 
 
@@ -14,7 +15,8 @@ export function New(){
                         <h1>Criar nota</h1>
                         <a href="/">voltar</a>
                     </header>
-                    <Input placeholder="Título"/>
+                    <Input placeholder="Título" />
+                    <Textarea placeholder="Observações" />
                 </Form>
             </main>
         </Container>
