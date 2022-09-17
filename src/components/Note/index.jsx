07a59e6a19a4bb31dad8a35cc3224dc36d344c27
Gabriel Assana <gabrielhhz2@gmail.com>
2022-09-17 14:10:06 -0300

    Criando arquivos de rotas e corrigindo bug das tags nas notas

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -6,16 +6,16 @@ export function Note({ data, ...rest }){
         <Container {...rest}>
             <h1>{data.title}</h1>
 
-            {
+            { 
                 data.tags && 
                 <footer>
                     {
-                        data.tags.map(tag => {
-                                <Tag 
-                                    key={ tag.id }  
-                                    title={ tag.name } 
-                                />
-                            })
+                        data.tags.map(tag => (
+                            <Tag 
+                                key={tag.id} 
+                                title={tag.name} 
+                            />
+                        ))
                     }
                 </footer>
             }
--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -3,8 +3,8 @@ import { Container } from './styles';
 export function Tag({ title, ...rest }) {
 
     return(
-        <Container {...rest} >
-            {title}
+        <Container {...rest}>
+            { title }
         </Container>
     );
 }
