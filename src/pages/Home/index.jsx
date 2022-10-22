ed6644ff102252d33ecfec5fc2676c61450ca2bf
Gabriel Assana <gabrielhhz2@gmail.com>
2022-10-22 12:53:29 -0300

    Carregando tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { FiPlus } from 'react-icons/fi';
 
 import { ButtonText } from '../../components/ButtonText';
@@ -5,9 +6,22 @@ import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Note } from '../../components/Note';
 import { Section } from '../../components/Section';
+import { api } from '../../services/api';
 import { Brand, Container, Content, Menu, NewNote, Search } from './styles';
 
 export function Home() {
+    const [tags, setTags] = useState([]);
+
+
+    useEffect(() =>{
+        async function fetchTags(){
+            const response = await api.get("/tags")
+            setTags(response.data)
+        }
+
+        fetchTags();
+    },[])
+
     return (
         <Container>
             <Brand>
@@ -17,9 +31,22 @@ export function Home() {
             <Header/>
 
             <Menu>
-                <li><ButtonText title="Todos" isActive/></li>
-                <li><ButtonText title="React"/></li>
-                <li><ButtonText title="Nodejs"/></li>
+                <li>
+                    <ButtonText 
+                        title="Todos" 
+                        isActive
+                    />
+                </li>
+                { 
+                    tags && tags.map(tag => (
+                        <li key={String(tag.id)}>
+                            <ButtonText 
+                                title={tag.name} 
+                                isActive
+                            />
+                        </li>
+                    ))
+                }
             </Menu>
 
             <Search>
