7130f54df2a5a4bc091260723424046791881251
Gabriel Assana <gabrielhhz2@gmail.com>
2022-10-26 14:13:50 -0300

    Pesquisar notas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -84,7 +84,7 @@ export function Home() {
             <Search>
                 <Input 
                     placeholder="Pesquisar pelo título" 
-                    onChange={() => setSearch(e.target.value)}
+                    onChange={(e) => setSearch(e.target.value)}
                 />
             </Search>
 
